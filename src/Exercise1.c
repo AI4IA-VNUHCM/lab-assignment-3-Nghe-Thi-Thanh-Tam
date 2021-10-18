a9a4eaea856a182e8aca873673f1781d732cbeba
Nghe Thi Thanh Tam <91868406+Nghe-Thi-Thanh-Tam@users.noreply.github.com>
2021-10-18 17:55:58 +0700

    update

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -16,8 +16,10 @@ void Ex1(int arr[], int n){
 	//Your codes here
     int check = 1;
 	for (int i=0; i<(n-1)/2; i++) 
-		if (arr[i] != arr[n-(1+i)])
+		if (arr[i] != arr[n-(1+i)]) {
 			check = 0;
+			break;
+		}
 	printf("%s", check? "symmetric" : "asymmetric");
 }
 
